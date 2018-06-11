7a785585f6e09f1af2e0de574a881e35f15fb4b8
Thomas Lever <thomaslever508@gmail.com>
2018-06-11 14:23:25 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -11,7 +11,6 @@ function callMe() {
 
 function crazy() {
   // fix the code in here
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
@@ -24,7 +23,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+
   }
 }
